be7ee418861aa8192c73ea0d1be701afccb2622b
tamara <tamara@iiga.studio>
2023-05-30 14:45:56 -0400

    Styling

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
     $(".delete-eBook").on("click", function(e){
-
         $target = $(e.target);
         const id = $target.attr("data-id");
 
@@ -9,13 +8,18 @@ $(document).ready(function(){
             type: "DELETE",
             url: "/eBook/" + id,
             success: function(response){
-
-                alert("Deleting Yoga eBook");
+                alert("Deleting eBook");
                 window.location.href="/";
             },
             error: function(err){
                 console.log(err);
             }
         })
-    })
+    });
+
+    // Handle click on description to expand or collapse
+    $(".expandable").on("click", function(e){
+        $(e.target).toggleClass("collapsed");
+    });
+
 });
